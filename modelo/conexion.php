35923c1e50f7a5e5d0d4d7ba093ee5c6c6d83755
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2019-11-08 12:00:12 -0500

    Update conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -5,13 +5,17 @@ class Conexion{
   private $link;
 
   public function __construct(){
+//     define('DB_HOST','209.45.50.210');
+//     define('DB_USER','userclient');
+//     define('DB_PASS','asociados517');
+//     define('DB_NAME','BDUS_CK000050_PRE00');
 
     /*$this->link = sqlsrv_connect("kunaqfactelec.cgjpxdkt8txc.sa-east-1.rds.amazonaws.com","sa_admin","asociados517");
 
     $this->nombreBD = $nombreBD;*/
 
-    $connection = array("Database"=>"BDUS_CK000050_0001","UID"=>"userclient","PWD"=>"asociados517",'CharacterSet' => 'UTF-8');
-    $this->link = sqlsrv_connect("KQ_ANALISTA\SQLSERVER",$connection);
+    $connection = array("Database"=>"BDUS_CK000050_PRE00","UID"=>"userclient","PWD"=>"asociados517",'CharacterSet' => 'UTF-8');
+    $this->link = sqlsrv_connect("209.45.50.210",$connection);
     if (!$this->link) {
         echo "Error: No se pudo conectar a Sql." . PHP_EOL;
         //echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
