232df024da56ac3439f2e8a39a675efa5e4fbd3f
Mirelly <mirellyagv@gmail.com>
2019-11-18 10:33:08 -0500

    con

--- extensiones/captcha/login.php
+++ extensiones/captcha/login.php
@@ -8,7 +8,7 @@ require_once "../../modelo/conexion.php";
    $db = new Conexion();                                             
 
     $sql = $db->consulta("SELECT * FROM scfma_usuario where cod_usuario = '$usuario' AND dsc_clave = '$pass' AND flg_activo = 'SI'");
-
+    echo "SELECT * FROM scfma_usuario where cod_usuario = '$usuario' AND dsc_clave = '$pass' AND flg_activo = 'SI'";
     $datos = array();
     while($key = $db->recorrer($sql)){
             $datos[] =  $key;
