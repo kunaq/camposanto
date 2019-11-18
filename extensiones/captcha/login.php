c7270c8496dbaf0a026b52ada5f6211e67575cc6
Mirelly <mirellyagv@gmail.com>
2019-11-18 10:44:52 -0500

    a ver

--- extensiones/captcha/login.php
+++ extensiones/captcha/login.php
@@ -10,24 +10,34 @@ require_once "../../modelo/conexion.php";
     $sql = $db->consulta("SELECT * FROM pr04_tab0002 where cod_usuario = '$usuario' AND dsc_clave = '$pass' AND flg_activo = 'SI'");
     // echo "SELECT * FROM scfma_usuario where cod_usuario = '$usuario' AND dsc_clave = '$pass' AND flg_activo = 'SI'";
     $datos = array();
-    echo count($db->recorrer($sql)).'ww';
+    // echo count($db->recorrer($sql)).'ww';
+    $cont = 0;
     while($key = $db->recorrer($sql)){
-            $datos[] =  $key;
-            $_SESSION['user'] =  $key['cod_usuario']; 
-            $bar = $key['dsc_usuario'];
-            $_SESSION['nombre'] = ucwords(strtolower($bar));             
-    }        
-
-    if ($sql) {
-       // $rows = rows( $sql );
-       // if($rows == true){
-       //  echo 'true';
-       // }
-       // else{
-       //  echo 'false';
-       // }
-      echo 'true';
+      $cont++;
     }
+    if($cont > 0){
+      while($key = $db->recorrer($sql)){
+        $datos[] =  $key;
+        $_SESSION['user'] =  $key['cod_usuario']; 
+        $bar = $key['dsc_usuario'];
+        $_SESSION['nombre'] = ucwords(strtolower($bar));
+        echo 'true';
+      }          
+    }else{
+      echo 'false';
+    }
+    
+
+    // if ($sql) {
+    //    // $rows = rows( $sql );
+    //    // if($rows == true){
+    //    //  echo 'true';
+    //    // }
+    //    // else{
+    //    //  echo 'false';
+    //    // }
+    //   echo 'true';
+    // }
 
     $db->liberar($sql);
  		$db->cerrar();  
