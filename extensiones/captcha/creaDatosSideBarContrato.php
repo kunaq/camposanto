4c0792f43111e85bae19bfc851e1d5fd80f513d3
sebastian1907 <sebastian19071997@hotmail.com>
2019-11-19 09:36:50 -0500

    array
    prueba array

--- extensiones/captcha/creaDatosSideBarContrato.php
+++ extensiones/captcha/creaDatosSideBarContrato.php
@@ -124,7 +124,7 @@ while($key = $db->recorrer($sql)){
                         </button>';
         }
 
-        $arrData = array('num_contrato'=> $numContrato, 'cod_servicio'=> $codServicio, 'dsc_cliente'=> $cliente, 'tipo_necesidad'=> $ttipoNec, 'fch_emision'=> $fechEmi, 'fch_activacion'=> $fechAct, 'fch_resolucion'=> $fechRes, 'fch_anulacion'=> $fechAnu, 'dsc_vendedor'=> $vendedor, 'tipo_servicio'=>$tipoServ, 'num_cuotas'=> $numCuotas, 'tasa_interes'=> $tasainteres, 'total'=>$total, 'buttons'=> $buttons);
+        $arrData = ['num_contrato'=> $numContrato, 'cod_servicio'=> $codServicio, 'dsc_cliente'=> $cliente, 'tipo_necesidad'=> $ttipoNec, 'fch_emision'=> $fechEmi, 'fch_activacion'=> $fechAct, 'fch_resolucion'=> $fechRes, 'fch_anulacion'=> $fechAnu, 'dsc_vendedor'=> $vendedor, 'tipo_servicio'=>$tipoServ, 'num_cuotas'=> $numCuotas, 'tasa_interes'=> $tasainteres, 'total'=>$total, 'buttons'=> $buttons];
     }
 
     echo json_encode($arrData);
