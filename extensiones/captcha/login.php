794c6917c44a99747867cd4f7ff4214df9524d0d
Mirelly <mirellyagv@gmail.com>
2019-11-29 11:33:49 -0500

    Update login.php

--- extensiones/captcha/login.php
+++ extensiones/captcha/login.php
@@ -20,9 +20,9 @@ require_once "../../modelo/conexion.php";
         $_SESSION['nombre'] = ucwords(strtolower($bar));
     }
     if($cont > 0){
-        return true;         
+        echo 'true';         
     }else{
-      return false;
+      echo 'false';
     }
     
 
