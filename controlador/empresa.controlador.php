7b8ee4b7b06bcbea3009049c1e7485c8eedcc560
Mirelly <mirellyagv@gmail.com>
2019-11-29 10:23:15 -0500

    inicio empresas

--- controlador/empresa.controlador.php
+++ controlador/empresa.controlador.php
@@ -2,117 +2,82 @@
 
 class ControladorEmpresa{
 
-	static public function ctrMostrarEmpresa($valor,$item){
-
-		$tabla = "scfma_empresa";
-
-		
-
-		$respuesta = ModeloEmpresa::mdlMostrarEmpresa($tabla,$valor,$item);
-
+	static public function ctrMostrarEmpresa($entrada){
+		$tabla1 = 'scfma_empresa';
+		$respuesta = ModeloEmpresa::mdlMostrarEmpresa($entrada,$tabla1);
 		return $respuesta;
-
-	}//function ctrPlantilla
+	}//function ctrMostrarEmpresa
 
 	static public function ctrPais(){
-
 		$respuesta = ModeloEmpresa::mdlPais();
-
 		return $respuesta;
 	}
 
 	static public function ctrestadocivil(){
-
 		$respuesta = ModeloEmpresa::mdlestadocivil();
-
 		return $respuesta;
 	}
 
 	static public function ctrSelects($tabla,$item1,$item2){
-
 		$respuesta = ModeloEmpresa::mdlSelects($tabla,$item1,$item2);
-
 		return $respuesta;
 	}
 
 	static public function ctrtipoPrograma(){
-
 		$respuesta = ModeloEmpresa::mdltipoPrograma();
-
 		return $respuesta;
 	}
 
 	static public function ctrtipoDoc(){
-
 		$respuesta = ModeloEmpresa::mdltipoDoc();
-
 		return $respuesta;
 	}
 
 	static public function ctrCamposanto(){
-
 		$respuesta = ModeloEmpresa::mdlCamposanto();
-
 		return $respuesta;
 	}
 
 	static public function ctrtipoCambio(){
-
 		$respuesta = ModeloEmpresa::mdltipoCambio();
-
 		return $respuesta;
 	}
 
 	static public function ctrcodVendedor(){
-
 		$respuesta = ModeloEmpresa::mdlcodVendedor();
-
 		return $respuesta;
 	}
 
 	static public function ctrdocCliente(){
-
 		$respuesta = ModeloEmpresa::mdldocCliente();
-
 		return $respuesta;
 	}
 
 	static public function ctrnomVendedor($cod){
-
 		$respuesta = ModeloEmpresa::mdlnomVendedor($cod);
-
 		return $respuesta;
 	}
-	static public function ctrtabVendedor(){
 
+	static public function ctrtabVendedor(){
 		$respuesta = ModeloEmpresa::mdltabVendedor();
-		//var_dump($respuesta);
 		return $respuesta;
 	}
 	static public function ctrtabCliente(){
-
 		$respuesta = ModeloEmpresa::mdltabCliente();
-
 		return $respuesta;
 	}
 	static public function ctrtabProspecto(){
-
 		$respuesta = ModeloEmpresa::mdltabProspecto();
-
 		return $respuesta;
 	}
 
 	static public function ctrtipoCom(){
-
 		$respuesta = ModeloEmpresa::mdltipoCom();
-
 		return $respuesta;
 	}
 
 	static public function ctrAnnoPeriodo(){
-
 		$respuesta = ModeloEmpresa::mdlAnnoPeriodo();
-
 		return $respuesta;
 	}
 
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once "extensiones/captcha/simple-php-captcha.php";
 require_once "funciones.php";
 require_once "controlador/plantilla.controlador.php";
 require_once "controlador/empresa.controlador.php";
+require_once "controlador/controlEmpresa.controlador.php";
 require_once "modelo/empresa.modelo.php";
 
 $plantilla = new ControladorPlantilla();
