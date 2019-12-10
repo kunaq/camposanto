7e101ec5acdeda8c3720a81c31633d6b91a5be97
Mirelly <mirellyagv@gmail.com>
2019-12-10 16:00:40 -0500

    Update notFound.php

--- vista/modulos/notFound.php
+++ vista/modulos/notFound.php
@@ -20,6 +20,7 @@
 	<div class="m-container m-container--fluid m-container--full-height m-page__container">
 		<div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
 			<div class="m-stack__item m-stack__item--center m-stack__item--middle m-stack__item--last">
+				<img src="vista/img/logo_SG5.png" style="background-color:white; height: 40px; border-radius: 6px;">
 				<span class="m-footer__copyright">
 					2019 &copy; Desarrollado por
 					<a href="http://www.kunaq.pe" class="m-link" target="_blank">
