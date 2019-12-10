d1d20b0d4589133e82818903963acfaf96f3de43
Mirelly <mirellyagv@gmail.com>
2019-12-10 11:08:51 -0500

    Update listaservicios.php

--- extensiones/calendario/listaservicios.php
+++ extensiones/calendario/listaservicios.php
@@ -157,7 +157,7 @@ $db = new Conexion();
 
              $eventos[] = array('id' => '', 'title' => $titulo1, 'titulo2' => $titulo2, 'description' => $description , 'start' => $date, 'allDay' => false, 'color' => $key['num_color'], 'textColor' => '#f8f9fa');
         }
-
+       var_dump($eventos);
        $arrayJson = json_encode($eventos, JSON_UNESCAPED_UNICODE);
        print_r($arrayJson);
 
