1a435831ecae2d4926aa49e3d7c287306c5c1526
Mirelly <mirellyagv@gmail.com>
2019-12-12 18:05:45 -0500

    Update modalConfiguraciones.php

--- vista/modulos/modals/modalConfiguraciones.php
+++ vista/modulos/modals/modalConfiguraciones.php
@@ -2,6 +2,7 @@
 <div class="modal fade" id="m_modal_configuraciones" tabindex="-1" role="dialog" aria-labelledby="configuracionesModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
+		<form id="configuraciones">
 			<div class="modal-header">
 				<h5 class="modal-title" id="configuracionesModalLabel">
 					Configuraciones
@@ -14,7 +15,6 @@
 			</div>
 			<div class="modal-body" >
 				<div class="m-scrollable" data-scrollbar-shown="true" data-scrollable="true" data-max-height="490">
-					<form id="configuraciones">
 					<div class="form-group row">
 						<div class="col-lg-4">
 							<label>Elija un avatar:</label>
@@ -144,20 +144,20 @@
 							</div>
 						</div>
 					</div>
-					<div class="form-group row">
-						<div class="col-md-12">
-							<p class="pull-right">
-								<button type="button" class="btn btn-metal">
-									Guardar cambios
-								</button>
-							</p>
-						</div>
-					</div>
-				</form>
 				</div>
 			</div>
 			<div class="modal-footer">
+				<div class="form-group row">
+					<div class="col-md-12">
+						<p class="pull-right">
+							<button type="button" class="btn btn-metal">
+								Guardar cambios
+							</button>
+						</p>
+					</div>
+				</div>
 			</div>
+		</form>
 		</div>
 	</div>
 </div>
