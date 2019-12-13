823071d4dd6f8a1f4a5d750c716864c23baf5161
Mirelly <mirellyagv@gmail.com>
2019-12-13 11:14:38 -0500

    privacidad

---
+++ vista/modulos/modals/modalPrivacidad.php
@@ -0,0 +1,69 @@
+
+<div class="modal fade" id="m_modal_PolPrivacidad" tabindex="-1" role="dialog" aria-labelledby="configuracionesModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+		<form id="configuraciones">
+			<div class="modal-header">
+				<h5 class="modal-title" id="configuracionesModalLabel">
+					POLÍTICA DE PRIVACIDAD
+				</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">
+						&times;
+					</span>
+				</button>
+			</div>
+			<div class="modal-body" >
+				<div class="m-scrollable" data-scrollbar-shown="true" data-scrollable="true" data-max-height="490">
+					<span>
+						El presente Política de Privacidad establece los términos en que Kunaq & Asociados S.A.C usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.
+					</span>
+					<br>
+					<h6>Información que es recogida</h6>
+					<br>
+					<span>
+						Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,  información de contacto como  su dirección de correo electrónica e información demográfica. Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.
+					</span>
+					<br>
+					<h6>Uso de la información recogida</h6>
+					<br>
+					<span>
+						Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios.  Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante para usted o que pueda brindarle algún beneficio, estos correos electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.
+						<br>
+						Kunaq & Asociados S.A.C está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que no exista ningún acceso no autorizado.
+					</span>
+					<br>
+					<h6>Cookies</h6>
+					<br>
+					<span>
+						Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener información respecto al tráfico web, y también facilita las futuras visitas a una web recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerte individualmente y por tanto brindarte el mejor servicio personalizado de su web.
+						<br>
+						Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis estadístico y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estás no dan acceso a información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente, visitas a una web . Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.
+					</span>
+					<br>
+					<h6>Control de su información personal</h6>
+					<br>
+					<span>
+						En cualquier momento usted puede restringir la recopilación o el uso de la información personal que es proporcionada a nuestro sitio web.  Cada vez que se le solicite rellenar un formulario, como el de alta de usuario, puede marcar o desmarcar la opción de recibir información por correo electrónico.  En caso de que haya marcado la opción de recibir nuestro boletín o publicidad usted puede cancelarla en cualquier momento.
+						<br>
+						Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial.
+						<br>
+						Kunaq & Asociados S.A.C se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.
+					</span>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<div class="form-group row">
+					<div class="col-md-12">
+						<p class="pull-right">
+							<button type="button" class="btn btn-metal">
+								Aceptar
+							</button>
+						</p>
+					</div>
+				</div>
+			</div>
+		</form>
+		</div>
+	</div>
+</div>
