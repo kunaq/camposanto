bf2bace41a23438b6e4960aa4db55b817036da05
Mirelly <mirellyagv@gmail.com>
2019-12-16 16:19:03 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -34,7 +34,7 @@ function creaTablaTrabajadoresArbVend(){
 	                $("#listaTrabArbVen").append(
 	                    '<li class="nav-item '+classPeriodo+' itemLista">'+
 	                        '<a href="#" class="btnVerTrabArbVen" codTrabajador="'+value['cod_trabajador']+'">'+
-	                        	'<div class="row" style = "'+color+'">'+
+	                        	'<div class="row" style = "color:'+color+'">'+
 									'<div class="col-md-4">'+value['cod_trabajador']+'</div>'+
 									'<div class="col-md-8">'+value['dsc_apellido_paterno']+' '+value['dsc_apellido_materno']+', '+value['dsc_nombres']+'</div>'+
 								'</div>'+
