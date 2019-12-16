114ebb5f4c9bb55310fbaf0f271ef95d2625a56f
Mirelly <mirellyagv@gmail.com>
2019-12-16 10:46:11 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -10,40 +10,40 @@ $("#cttoResArbVen").on('click', function(){
 
 function creaTablaTrabajadoresArbVend(){
 	// alert('llego');
-	// $('#tableTarbajadorArbVen').html('<div class="loader"></div>');
-        // $.ajax({
-        //     type:'POST',
-        //     url: 'ajax/datatable-trabajadorArbVen.ajax.php',
-        //     dataType: 'json',
-        //     data: {'entrada':'verTrabajadores'},
-        //     success : function(respuesta){
-        //     	console.log(respuesta);
-        //         // $("#divTablaProspectos").html(respuesta);
-        //         // $('#tableTarbajadorArbVen').DataTable({
-        //         //     "searching": false,
-        //         //     "info": false
-        //         // });
-        //     }
-        // });
-	$("#tableTarbajadorArbVen").DataTable({
-		"ajax": "ajax/datatable-trabajadorArbVen.ajax.php?entrada=verTrabajadores",
-	    "deferRender": true,
-		"retrieve": true,
-		"processing": true,
-		"language" : {
-	      "url": "spanish.json"
-	  	},
-	  	'columnDefs': [
-			{
-				targets: [0],
-				className: "text-center codTrabajador"
-			},
-			{
-				targets: [1],
-				className: ""
-			}
-		]	
-	});
+	$('#tableTarbajadorArbVen').html('<div class="loader"></div>');
+        $.ajax({
+            type:'POST',
+            url: 'ajax/datatable-trabajadorArbVen.ajax.php',
+            dataType: 'json',
+            data: {'entrada':'verTrabajadores'},
+            success : function(respuesta){
+            	console.log(respuesta);
+                $("#divTablaProspectos").html(respuesta);
+                $('#tableTarbajadorArbVen').DataTable({
+                    "searching": false,
+                    "info": false
+                });
+            }
+        });
+	// $("#tableTarbajadorArbVen").DataTable({
+	// 	"ajax": "ajax/datatable-trabajadorArbVen.ajax.php?entrada=verTrabajadores",
+	//     "deferRender": true,
+	// 	"retrieve": true,
+	// 	"processing": true,
+	// 	"language" : {
+	//       "url": "spanish.json"
+	//   	},
+	//   	'columnDefs': [
+	// 		{
+	// 			targets: [0],
+	// 			className: "text-center codTrabajador"
+	// 		},
+	// 		{
+	// 			targets: [1],
+	// 			className: ""
+	// 		}
+	// 	]	
+	// });
 }
 
 creaTablaTrabajadoresArbVend();
