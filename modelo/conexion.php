62a33d2c767863900084c2b35b20b27ce7252c97
Mirelly <mirellyagv@gmail.com>
2019-12-17 10:18:19 -0500

    Update conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -2,47 +2,27 @@
 class Conexion{
   private $link;
   public function __construct(){
-    define('DB_HOST','209.45.50.210');
-    define('DB_USER','userclient');
-    define('DB_PASS','asociados517');
-    define('DB_NAME','BDUS_CK000050_CRE01');
-    //  $connection = array("Database"=>"BDUS_CK000050_PRE00","UID"=>"userclient","PWD"=>"asociados517",'CharacterSet' => 'UTF-8');
-    // $this->link = mssql_connect("209.45.50.210",$connection);
-    $this->link = mssql_connect(DB_HOST,DB_USER,DB_PASS);
-    if(!$this->link){
-          echo 'No se pudo conectar';
-          exit;
-      }
-      if(!mssql_select_db(DB_NAME, $this->link)){
-          echo 'No se pudo conectar';
-          exit;
-      }
+    /*$this->link = sqlsrv_connect("kunaqfactelec.cgjpxdkt8txc.sa-east-1.rds.amazonaws.com","sa_admin","asociados517");
+    $this->nombreBD = $nombreBD;*/
+    $connection = array("Database"=>"BDUS_CK000050_0001","UID"=>"userclient","PWD"=>"asociados517",'CharacterSet' => 'UTF-8');
+    $this->link = sqlsrv_connect("KQ_ANALISTA\SQLSERVER",$connection);
+    if (!$this->link) {
+        echo "Error: No se pudo conectar a Sql." . PHP_EOL;
+        //echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuración: " . sqlsrv_errors() . PHP_EOL;
+        exit;
+    }
   }//function  __construct
   public function consulta($query){
-    return mssql_query($query);
+    return sqlsrv_query($this->link,$query);
   }
   public function recorrer($query){
-    return mssql_fetch_array($query);
-  }
-  public function rows($query){
-      return mssql_num_rows($query);
+    return sqlsrv_fetch_array($query,SQLSRV_FETCH_ASSOC);
   }
   public function liberar($query){
-    return mssql_free_result($query);
+    return sqlsrv_free_stmt($query);
   }
   public function cerrar(){
-    return mssql_close($this->link);
+    return sqlsrv_close($this->link);
   }
-  public function inicioTransaccion(){
-    return true;
-  }
-  public function beginTransaction(){
-    return true;
-  }
-  public function commit(){
-    return true;
-  }
-  public function rollback(){
-    return true;
-  }
-}//class Conexion
+}
