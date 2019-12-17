56b2694ac53515cf1befa39b5b1483b76c2e85ad
sebastian1907 <sebastian19071997@hotmail.com>
2019-12-17 11:13:14 -0500

    Update ObtieneDatosProspecto.php

--- extensiones/captcha/ObtieneDatosProspecto.php
+++ extensiones/captcha/ObtieneDatosProspecto.php
@@ -21,9 +21,7 @@ while($key = $db->recorrer($sql)){
         $provincia = $key['cod_provincia'];
         $distrito = $key['cod_distrito'];
         $telefono1 = $key['dsc_telefono_1'];
-        echo $telefono1;
         $telefono2 = $key['dsc_telefono_2'];
-        echo $telefono2;
 
         if ($key['fch_registro'] == NULL) {
             $fechReg = "-";
@@ -33,9 +31,8 @@ while($key = $db->recorrer($sql)){
         }
 
         $usuario = $key['cod_usuario'];
-        echo $usuario;
 
-        $arrData = array('tipoDoc'=> $tipoDoc, 'numDoc'=> $numDoc, 'juridico'=> $juridico, 'apePaterno'=> utf8_encode($apePaterno), 'apeMaterno'=> utf8_encode($apeMaterno), 'nombre'=> utf8_encode($nombre), 'direccion'=> utf8_encode($direccion), 'pais'=> $pais, 'departamento'=> $departamento, 'provincia'=>$provincia, 'distrito'=> $distrito, 'telefono1'=> $telefono1, 'telefono2'=>$telefono2, 'fechReg'=> $fechReg, 'usuario'=>$usuario);
+        $arrData = array('tipoDoc'=> $tipoDoc, 'numDoc'=> $numDoc, 'juridico'=> $juridico, 'apePaterno'=> utf8_encode($apePaterno), 'apeMaterno'=> utf8_encode($apeMaterno), 'nombre'=> utf8_encode($nombre), 'direccion'=> utf8_encode($direccion), 'pais'=> $pais, 'departamento'=> $departamento, 'provincia'=>$provincia, 'distrito'=> $distrito, 'telefono1'=> $telefono1, 'telefono2'=>$telefono2, 'fechReg'=> $fechReg, 'usuario'=> utf8_encode($usuario));
     }
 
     echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
