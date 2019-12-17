efdb8a730568ca86db795ba2a8cf6b92aa27d402
sebastian1907 <sebastian19071997@hotmail.com>
2019-12-17 11:02:59 -0500

    Update ObtieneDatosProspecto.php

--- extensiones/captcha/ObtieneDatosProspecto.php
+++ extensiones/captcha/ObtieneDatosProspecto.php
@@ -4,16 +4,17 @@ require_once "../../funciones.php";
 
 $codPro = $_POST['codPro'];
 
-echo $codPro;
-
 $db = new Conexion();
 
 $sql = $db->consulta("SELECT * FROM vtaca_prospecto_venta WHERE cod_prospecto = '$codPro'");
 
 while($key = $db->recorrer($sql)){
         $tipoDoc = $key['cod_tipo_documento'];
+        echo $tipoDoc;
         $numDoc = $key['dsc_documento'];
+        echo $numDoc;
         $juridico = $key['flg_juridico'];
+        echo $juridico;
         $apePaterno = $key['dsc_apellido_paterno'];
         $apeMaterno = $key['dsc_apellido_materno'];
         $nombre = $key['dsc_nombre'];
