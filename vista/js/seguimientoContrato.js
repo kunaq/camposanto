c3f7b61b503a2d663d336854e472ab72de9bc077
Mirelly <mirellyagv@gmail.com>
2019-12-19 18:00:28 -0500

    Update seguimientoContrato.js

--- vista/js/seguimientoContrato.js
+++ vista/js/seguimientoContrato.js
@@ -24,15 +24,12 @@ function init(){
 
 init();
 
-function getQueryVariable(variable) {
-   var query = window.location.search.substring(1);
-   var vars = query.split("&");
-   for (var i=0; i < vars.length; i++) {
-       var pair = vars[i].split("=");
-       if(pair[0] == variable) {
-       	console.log(pair[1]);
-           return pair[1];
-       }
-   }
-   return false;
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+var codCtto = getParameterByName('codCtto');
+alert(codCtto);
