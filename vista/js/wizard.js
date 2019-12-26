f85151914b39e9dc1d8abef0d5b5c580134ab084
Mirelly <mirellyagv@gmail.com>
2019-12-26 13:20:41 -0500

    fecha

--- vista/js/wizard.js
+++ vista/js/wizard.js
@@ -1332,6 +1332,7 @@ function eliminaServicio(id){
     $("#interes").val('').trigger('change');
     document.getElementById("imp_saldo_foma").value = tot;
     $("#cuota_FOMA").val('').trigger('change');
+    var ldt_fch_ven = $('#m_datepicker_1').datepicker("getDate"); 
     aux_dia = ldt_fch_ven.getDate();
     aux_mes1 = ldt_fch_ven.setMonth(ldt_fch_ven.getMonth() + 1);
     var aux_mes = ldt_fch_ven.getMonth();
