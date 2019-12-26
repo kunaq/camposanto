529d4cca1c86f3b45cc175557baf9b74ff772102
Mirelly <mirellyagv@gmail.com>
2019-12-26 10:13:32 -0500

    Update tablaDescuentos.php

--- extensiones/captcha/tablaDescuentos.php
+++ extensiones/captcha/tablaDescuentos.php
@@ -9,8 +9,8 @@ require_once "../../modelo/conexion.php";
               inner join vtade_tipo_descuento
               ON vtama_tipo_descuento.cod_tipo_descuento = vtade_tipo_descuento.cod_tipo_descuento 
               WHERE flg_activo = 'SI' 
-              AND CONVERT(DATE,vtade_tipo_descuento.fch_inicio) <= CONVERT(DATE, '13/10/2019')
-              AND  CONVERT(DATE,vtade_tipo_descuento.fch_fin) >= CONVERT(DATE, '13/06/2019')");
+              AND vtade_tipo_descuento.fch_inicio <=  '13/10/2019'
+              AND  vtade_tipo_descuento.fch_fin >=  '13/06/2019'");
 
                 $datos = array();
                 //$datos = '';
