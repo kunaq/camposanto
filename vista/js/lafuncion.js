686f85cbf0567a9b6f86a2fafde7d3be0671bc98
Mirelly <mirellyagv@gmail.com>
2019-12-27 17:57:58 -0500

    Update lafuncion.js

--- vista/js/lafuncion.js
+++ vista/js/lafuncion.js
@@ -148,7 +148,7 @@ function cambiaTodo()
        
        if(is_flg_generacion_ee == 'SI')          
        {  
-          if(ls_flg_ds == 'SI' && ls_flg_ssff == 'SI')
+          if(ls_flg_ds == 'SI' || ls_flg_ssff == 'SI')
           {
             lde_afecto_dscto = lde_afecto_dscto + document.getElementById("numA_"+ls_servicio_main).value;
           }
@@ -190,7 +190,7 @@ function cambiaTodo()
                     
          if(is_flg_generacion_ee == 'SI')               
          {    
-           if(ls_flg_ds == 'SI' && ls_flg_ssff == 'SI')
+           if(ls_flg_ds == 'SI' || ls_flg_ssff == 'SI')
            {
               lde_afecto_dscto = lde_afecto_dscto + (document.getElementById("numA_"+ls_servicio_main).value - document.getElementById("numF_"+ls_servicio_main).value);
            }
