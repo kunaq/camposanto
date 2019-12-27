1688dd09d6724f7774fbb86bb4d9799852d763a1
Mirelly <mirellyagv@gmail.com>
2019-12-27 16:47:06 -0500

    Update lafuncion.js

--- vista/js/lafuncion.js
+++ vista/js/lafuncion.js
@@ -299,7 +299,7 @@ function cambiaTodo()
         //console.log(lde_afecto_dscto);
         lde_valor = (( lde_afecto_dscto * lde_valor ) / 100);   //-------Aqui lde_afecto_dscto es 0 siempre!!
       }    
-
+     console.log('lde_valor',lde_valor);
       // -- Seteo -- //
       
       //tab_1.tp_7.dw_dscto.SetItem(li_i, "imp_monto", lde_valor);
