fae251b42e86ceac8320394781999cea48c700ff
sebastian1907 <sebastian19071997@hotmail.com>
2019-12-30 12:26:57 -0500

    verificar Prospecto Existente

---
+++ extensiones/funciones/verificarProspectoExistente.php
@@ -0,0 +1,42 @@
+<?php 
+require_once "../../modelo/conexion.php";
+
+$tipoDoc = $_POST['tipoDoc'];
+$numDoc = $_POST['numDoc'];
+
+$db = new Conexion();
+
+$sql = $db->consulta("SELECT cod_estado FROM vtaca_prospecto_venta WHERE cod_tipo_documento = '$tipoDoc' AND dsc_documento = '$numDoc';");
+
+// $estado = "";
+
+if ($rows = $db->row($sql)) {
+     
+    while($key = $db->recorrer($sql)){
+
+        if ($key['cod_estado'] == "ACT") {
+            $estado = "activo";
+        }elseif ($key['cod_estado'] == "VTA") {
+            $estado = "cierre";
+        }elseif ($key['cod_estado'] == "CAD") {
+            $estado = "caduco";
+        }elseif($key['cod_estado'] == "TRU"){
+            $estado = "trunco";
+        }
+    }
+
+    $code = "1";
+
+	$arrData = array('cod'=> $code, 'estado'=>$estado);
+	echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+
+    $db->liberar($sql);
+    $db->cerrar();
+}else{
+	$code = "0";
+	$arrData = array('cod'=> $code);
+	echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+}
+
+
+ ?>
