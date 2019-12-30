f01343eab69add607dda56e633a39a1edd7c3038
Mirelly <mirellyagv@gmail.com>
2019-12-30 09:57:44 -0500

    nueva configuracion arbol de vendedores

---
+++ vista/modulos/modals/modalNvoConfigArbVend.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="m_modal_nvoConfigArbVen" tabindex="-1" role="dialog" aria-labelledby="configuracionesModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+		<form id="configuraciones">
+			<div class="modal-header">
+				<h5 class="modal-title" id="configuracionesModalLabel">
+					Configuración árbol vendedor
+				</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">
+						&times;
+					</span>
+				</button>
+			</div>
+			<div class="modal-body" >
+				<div class="m-scrollable" data-scrollbar-shown="true" data-scrollable="true" data-max-height="490">
+					
+					
+				</div>
+			</div>
+			<div class="modal-footer">
+				<div class="form-group row">
+					<div class="col-md-12">
+						<p class="pull-right">
+							<button type="button" class="btn btn-sm btnGuardarKqPst" title="Guardar" id="">Aceptar</button>	
+							<button type="button" class="btn btn-sm btn-danger close" data-dismiss="modal" title="Cerrar" id="">Cancelar</button>
+						</p>
+					</div>
+				</div>
+			</div>
+		</form>
+		</div>
+	</div>
+</div>
