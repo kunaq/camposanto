20007da49a3185a7c886d165c57101de5ddf98ca
Mirelly <mirellyagv@gmail.com>
2019-12-31 11:55:59 -0500

    Update wizard.php

--- vista/modulos/wizard.php
+++ vista/modulos/wizard.php
@@ -1460,7 +1460,7 @@
 									</a>
 								</div>
 								<div class="col-lg-4 m--align-right">
-									<a href="#" class="btn btnGuardarKqPst m-btn m-btn--custom m-btn--icon" data-wizard-action="submit">
+									<a href="#" class="btn btnGuardarKqPst m-btn m-btn--custom m-btn--icon" data-wizard-action="submit" onclick="grabaTemporal();">
 										<span>
 											<i class="la la-check"></i>
 											&nbsp;&nbsp;
