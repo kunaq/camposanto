4583eb5a00001c0ea3f84b099fe1e4869d41bbf8
sebastian1907 <sebastian19071997@hotmail.com>
2019-12-31 10:44:29 -0500

    Update buscaCliente.php

--- extensiones/captcha/buscaCliente.php
+++ extensiones/captcha/buscaCliente.php
@@ -10,7 +10,7 @@ require_once "../../modelo/conexion.php";
        	$sql = $db->consulta("SELECT c.cod_cliente, c.dsc_cliente, c.dsc_documento, c.cod_tipo_documento, c.flg_juridico, c.dsc_telefono_1, d.dsc_direccion, c.cod_cliente FROM vtama_cliente c INNER JOIN vtade_cliente_direccion d ON c.cod_cliente=d.cod_cliente where c.cod_cliente = '$cod' OR c.dsc_documento = '$cod' AND d.flg_comprobante='SI';");
 
 
-$respuesta = "";
+// $respuesta = "";
 
       if ($rows = $db->rows($sql)) {
           
