3b5533343e145bc32cbf20c7d8c8d35bbc98ca40
Mirelly <mirellyagv@gmail.com>
2020-01-03 11:41:12 -0500

    otra vez espacio

--- controlador/wizard.controlador.php
+++ controlador/wizard.controlador.php
@@ -47,7 +47,6 @@ class ControladorWizard{
 		$fecha = date('Y-m-d');
 		$hora = date('H:i:s');
 		$fechaActual = $fecha.' '.$hora;
-		$espacio = split('/',$_POST['espacio']);
 		$datos = array("a_usuario" => $_SESSION["user"],
 						"as_cliente" => $_POST['cod_cliente'],
 						"as_contrato_base" => NULL,
@@ -64,11 +63,11 @@ class ControladorWizard{
 						"as_area" => $_POST['area'],
 						"as_eje_horizontal" => $_POST['ejex'],
 						"as_eje_vertical" => $_POST['ejey'],
-						"as_tipo_espacio" => $espacio[0],
+						"as_tipo_espacio" => $_POST['tipoEspacio'],
 						"as_convenio" => $_POST['endoso'],
 						"as_moneda" => 'SOL',
 						"as_moneda_comprob" => NULL,
-						"as_espacio" => $espacio[2],
+						"as_espacio" => $_POST['espacio'],
 						"as_tipo_necesidad" => $_POST['tipoNec'],
 						"adt_fch_emision" => $fechaActual,
 						"ade_imp_cuoi" => $_POST['importeCUI'] ,
--- vista/js/lafuncion.js
+++ vista/js/lafuncion.js
@@ -2487,7 +2487,10 @@ function grabaTemporal(){
              if (response == 1) {
                j++;
                if (j == rowLength) {
-                console.log("aca");
+                // console.log("aca");
+                var aux = document.getElementById('espacio').value;
+                var tipo_espacio = aux.split("/")[0];
+                var espacio = aux.split("/")[2];
                 var cod_cliente = document.getElementById('cod_cliente').value;
                 var tipo_recaudacion = document.getElementById('tipPro').value;
                 var camposanto = document.getElementById('camposanto').value;
@@ -2495,10 +2498,8 @@ function grabaTemporal(){
                 var area = document.getElementById('area').value;
                 var eje_x = document.getElementById('ejex').value;
                 var eje_y = document.getElementById('ejey').value;
-                var espacio = document.getElementById('espacio').value;
                 var ultimoEnd = document.getElementById("bodyCobertura").lastChild;
                 var endoso = ultimoEnd.id;
-                var tipo_espacio = document.getElementById('tipoEspacio').value;
                 var tipo_necesidad = document.getElementById('tiponec').value;
                 var imp_cuoi = pasaAnumero(document.getElementById('importeCUI').value);
                 var nuevo_ctt = document.getElementById('flagNvoCtto').value;
