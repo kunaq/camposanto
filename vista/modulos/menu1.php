4d0d7232e801952b1411883b74369c24fc439d0b
Mirelly <mirellyagv@gmail.com>
2020-01-07 13:06:18 -0500

    Update menu1.php

--- vista/modulos/menu1.php
+++ vista/modulos/menu1.php
@@ -6,7 +6,7 @@
 	<!-- BEGIN: Aside Menu -->
 	<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="0" m-menu-dropdown-timeout="500">
 		<ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-			<li class="m-menu__item">
+			<li class="m-menu__item m-menu__item--submenu">
 				<a  href="inicio2" class="m-menu__link m-menu__toggle">
 					<i class="m-menu__link-icon la la-home"></i>
 					<span class="m-menu__link-text">
