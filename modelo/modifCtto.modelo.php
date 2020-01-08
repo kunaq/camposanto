565afefb69adeab5e34647359d076528af6acb70
Mirelly <mirellyagv@gmail.com>
2020-01-08 15:30:04 -0500

    Update modifCtto.modelo.php

--- modelo/modifCtto.modelo.php
+++ modelo/modifCtto.modelo.php
@@ -5,7 +5,7 @@ class ModeloModifCtto{
 
 	static public function mdlBuscaCttos($tabla,$codCtto){
 		$db = new Conexion();
-		$sql = $db->consulta("SELECT * FROM $tabla where cod_contrato = '$codCtto'");
+		$sql = $db->consulta("SELECT * FROM $tabla where cod_contrato LIKE '%$codCtto%'");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
 	    		$datos[] = arrayMapUtf8Encode($key);
