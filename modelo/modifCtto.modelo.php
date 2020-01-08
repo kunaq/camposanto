f308fd3ec4f9fd6af32d78d4d0fe1fae84f1af6d
Mirelly <mirellyagv@gmail.com>
2020-01-08 17:14:29 -0500

    Update modifCtto.modelo.php

--- modelo/modifCtto.modelo.php
+++ modelo/modifCtto.modelo.php
@@ -6,6 +6,7 @@ class ModeloModifCtto{
 	static public function mdlBuscaCttos($tabla,$tabla2,$codCtto){
 		$db = new Conexion();
 		$sql = $db->consulta("SELECT $tabla.*, $tabla2.dsc_cliente, $tabla3.* FROM $tabla INNER JOIN $tabla2 ON $tabla.cod_cliente = $tabla2.cod_cliente INNER JOIN $tabla3 ON $tabla.cod_contrato = $tabla3.cod_contrato WHERE $tabla.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10))");
+		echo "SELECT $tabla.*, $tabla2.dsc_cliente, $tabla3.* FROM $tabla INNER JOIN $tabla2 ON $tabla.cod_cliente = $tabla2.cod_cliente INNER JOIN $tabla3 ON $tabla.cod_contrato = $tabla3.cod_contrato WHERE $tabla.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10))";
 		$datos = array();
     	while($key = $db->recorrer($sql)){
 	    		$datos[] = arrayMapUtf8Encode($key);
