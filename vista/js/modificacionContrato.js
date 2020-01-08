5f8c6b068be2b5b720ef2a4782696de53235e971
Mirelly <mirellyagv@gmail.com>
2020-01-08 16:17:34 -0500

    Update modificacionContrato.js

--- vista/js/modificacionContrato.js
+++ vista/js/modificacionContrato.js
@@ -29,6 +29,7 @@ function llenaDatos(codCtto){
         data: { 'accion' : 'conCodigo', 'codCtto' : codCtto },
         success : function(respuesta){
         	console.log('con codigo',respuesta);
+        	console.log('tipo_programa',respuesta[0]['cod_tipo_programa'])
         	$("#tipoPrograma option[value='"+respuesta[0]['cod_tipo_programa']+"']").attr("selected",true);
         }
     });
