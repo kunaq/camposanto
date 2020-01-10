c0c9fb2421e1de65b31d649f04f55ca79fc12fcc
Mirelly <mirellyagv@gmail.com>
2020-01-10 09:36:11 -0500

    Update modifCtto.modelo.php

--- modelo/modifCtto.modelo.php
+++ modelo/modifCtto.modelo.php
@@ -19,7 +19,7 @@ class ModeloModifCtto{
 	static public function mdlBuscaDatosServicio($tabla,$codCtto,$num_servicio){
 		$db = new Conexion();
 		$sql = $db->consulta("SELECT * FROM $tabla WHERE cod_contrato  LIKE (RIGHT('0000000000'+'$codCtto',10)) AND num_servicio = $num_servicio");
-		echo "SELECT * FROM $tabla WHERE cod_contrato = '$codCtto' AND num_servicio = $num_servicio";
+		// echo "SELECT * FROM $tabla WHERE cod_contrato = '$codCtto' AND num_servicio = $num_servicio";
 		$datos = arrayMapUtf8Encode($db->recorrer($sql));
 		return $datos;
 		$db->liberar($sql);
