01185cb465af7c4d8affdc85906fabd752ce72a7
sebastian1907 <sebastian19071997@hotmail.com>
2020-01-13 14:49:49 -0500

    Update segCtt.modelo.php

--- modelo/segCtt.modelo.php
+++ modelo/segCtt.modelo.php
@@ -147,7 +147,7 @@ class ModeloSegContrato{
 	        if ($key['fch_vencimiento'] == NULL) {
 	            $fchVencimiento = "-";
 	        }else{
-	            $fchVencimiento = $key['fch_vencimiento']['date'];
+	            $fchVencimiento = $key['fch_vencimiento'];
 	        }
 	        // -------- Condicional para campos NULL de fch_cancelacion -------- //
 	        if ($key['fch_cancelacion'] == NULL) {
