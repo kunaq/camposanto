45206f3c5f7ff2cd79e06081a69642c0b8280912
sebastian1907 <sebastian19071997@hotmail.com>
2020-01-23 17:44:17 -0500

    wqe

---
+++ vista/modulos/modals/modalTablaComprobante.php
@@ -0,0 +1,73 @@
+
+<div class="modal fade" id="m_modal_tabla_comprobante" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">
+					Detalle de distribución de comprobante
+				</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">
+						&times;
+					</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="row">
+					<div class="col-lg-12">
+						<table class="table">
+							<thead>
+								<th>Tipo de Comprobante</th>
+								<th>N° de Comprobante</th>
+								<th>Fecha de Emisión</th>
+								<th>Localidad</th>
+								<th>Tipo Contrato</th>
+								<th>Tipo Programa</th>
+								<th>Contrato</th>
+								<th>Ref.</th>
+								<th>Cuota</th>
+								<th>M</th>
+								<th>Importe</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+							</tbody>
+							<tfoot>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td id="imp_total_footer"></td>
+								</tr>
+							</tfoot>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">
+					Aceptar
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
