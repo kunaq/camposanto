2e5f722172c355ddffcfed1027f00bb1ea3c7a9b
Mirelly <mirellyagv@gmail.com>
2020-02-14 17:19:05 -0500

    Update getPeriod.php

--- extensiones/captcha/getPeriod.php
+++ extensiones/captcha/getPeriod.php
@@ -1,6 +1,5 @@
 <?php
 require_once "../../modelo/conexion.php";
-require_once "../../funciones.php";
 
    $fechaRes = date($_GET['fechaRes']);
 
@@ -8,13 +7,12 @@ require_once "../../funciones.php";
 
     $sql = $db->consulta("SELECT num_anno, cod_tipo_periodo, cod_periodo FROM vtama_periodo where fch_inicio <='$fechaRes' AND fch_fin >='$fechaRes'");
 
-    $arrData = arrayMapUtf8Encode($db->recorrer($sql));
     // $datos = array();
 
-		// while($key = $db->recorrer($sql)){
-  //           $arrData = array('num_anno'=> $key['num_anno'], 'tipo_periodo'=> $key['cod_tipo_periodo'], 'periodo'=> $key['cod_periodo']);        
-		// }        
-		print_r(json_encode($arrData));
-    	$db->liberar($sql);
+		while($key = $db->recorrer($sql)){
+            $arrData = array('num_anno'=> $key['num_anno'], 'tipo_periodo'=> $key['cod_tipo_periodo'], 'periodo'=> $key['cod_periodo']);        
+		}        
+		echo json_encode($arrData);
+    $db->liberar($sql);
  		$db->cerrar();  
 ?>
