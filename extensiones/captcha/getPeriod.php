fab2514dedd7e7eb8f5549d15a4c0863212f5c78
Mirelly <mirellyagv@gmail.com>
2020-02-17 10:09:45 -0500

    Update getPeriod.php

--- extensiones/captcha/getPeriod.php
+++ extensiones/captcha/getPeriod.php
@@ -6,14 +6,19 @@ require_once "../../modelo/conexion.php";
     $db = new Conexion();                                             
 
     $sql = $db->consulta("SELECT num_anno, cod_tipo_periodo, cod_periodo, fch_inicio, fch_fin FROM vtama_periodo where num_anno = 2020");
-    echo  "SELECT num_anno, cod_tipo_periodo, cod_periodo, fch_inicio, fch_fin FROM vtama_periodo where num_anno = 2020";
+    // echo  "SELECT num_anno, cod_tipo_periodo, cod_periodo, fch_inicio, fch_fin FROM vtama_periodo where num_anno = 2020";
 
     // $datos = array();
     	// $arrData = arrayMapUtf8Encode($db->recorrer($sql));
-		while($key = $db->recorrer($sql)){
-            $arrData = array('num_anno'=> $key['num_anno'], 'tipo_periodo'=> $key['cod_tipo_periodo'], 'periodo'=> $key['cod_periodo']);        
-		}        
-		echo json_encode($arrData);
+		// while($key = $db->recorrer($sql)){
+  //           $arrData = array('num_anno'=> $key['num_anno'], 'tipo_periodo'=> $key['cod_tipo_periodo'], 'periodo'=> $key['cod_periodo']);        
+		// }        
+		// echo json_encode($arrData);
+		$datos = array();
+    	while($key = $db->recorrer($sql)){
+	    		$datos[] = arrayMapUtf8Encode($key);
+			}
+		print_r($datos);
     $db->liberar($sql);
  		$db->cerrar();  
 ?>
