481a6066c82033d9c3de6b9b4daad974ba69ed34
Mirelly <mirellyagv@gmail.com>
2020-02-17 09:49:41 -0500

    Update getPeriod.php

--- extensiones/captcha/getPeriod.php
+++ extensiones/captcha/getPeriod.php
@@ -6,6 +6,7 @@ require_once "../../modelo/conexion.php";
     $db = new Conexion();                                             
 
     $sql = $db->consulta("SELECT num_anno, cod_tipo_periodo, cod_periodo FROM vtama_periodo where fch_inicio <='$fechaRes' AND fch_fin >='$fechaRes'");
+    echo  "SELECT num_anno, cod_tipo_periodo, cod_periodo FROM vtama_periodo where fch_inicio <='$fechaRes' AND fch_fin >='$fechaRes'";
 
     // $datos = array();
 
