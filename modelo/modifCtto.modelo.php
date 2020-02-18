03b12d98e5a8c1ab62c9c8543f63a513c1f0217c
Mirelly <mirellyagv@gmail.com>
2020-02-18 11:27:13 -0500

    Update modifCtto.modelo.php

--- modelo/modifCtto.modelo.php
+++ modelo/modifCtto.modelo.php
@@ -205,8 +205,6 @@ class ModeloModifCtto{
 	static public function mdlBuscaBeneficiario($tablaBen,$datos){
 		$db = new Conexion();
 		$sql = $db->consulta("SELECT * FROM $tablaBen WHERE (cod_contrato LIKE (RIGHT('0000000000'+'".$datos['codCtto']."',10)) AND cod_localidad = ".$datos['localidad'].")");
-		echo "SELECT * FROM $tablaBen WHERE (cod_contrato LIKE (RIGHT('0000000000'+'".$datos['codCtto']."',10)) AND cod_localidad = ".$datos['localidad'].")";
-		$datos = array();
     	while($key = $db->recorrer($sql)){
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
