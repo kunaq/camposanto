5e6d5dcdf921be62689620cc8ecbe0daca02d8e5
sebastian1907 <sebastian19071997@hotmail.com>
2020-02-20 14:34:48 -0500

    Update prospecto.modelo.php

--- modelo/prospecto.modelo.php
+++ modelo/prospecto.modelo.php
@@ -12,7 +12,7 @@ class ModeloProspecto{
         LEN(cod_prospecto)-CHARINDEX('T', cod_prospecto))) AS ultimo_registro
         FROM vtaca_prospecto_venta ORDER BY cod_prospecto DESC;");
 
-		$ultimoPros = "";
+		// $ultimoPros = "";
 
 		while($key = $db->recorrer($sql)){
 		    $ultimoPros = $key['ultimo_registro'];
@@ -43,7 +43,7 @@ class ModeloProspecto{
 
 		    $consulta = "INSERT INTO vtaca_prospecto_venta(cod_prospecto, dsc_apellido_paterno, dsc_apellido_materno, dsc_nombre, flg_juridico, cod_tipo_documento, dsc_documento, dsc_prospecto, cod_pais, cod_departamento, cod_provincia, cod_distrito, dsc_direccion, dsc_telefono_1, dsc_telefono_2, cod_origen, cod_calificacion, dsc_observaciones, fch_registro, cod_usuario, cod_consejero, cod_grupo, cod_supervisor, cod_jefeventas, cod_estado, flg_cambio_activo) VALUES('$codPro', '".$datos['apePaterno']."', '".$datos['apeMaterno']."', '".$datos['nombre']."', '".$datos['juridico']."', '".$datos['tipoDoc']."', '".$datos['numDoc']."', '$dsc_prospecto', '".$datos['pais']."', '".$datos['departamento']."', '".$datos['provincia']."', '".$datos['distrito']."', '".$datos['direccion']."', '".$datos['telefono1']."', '".$datos['telefono2']."', '".$datos['origen']."', '".$datos['calificacion']."', '".$datos['observacion']."', '".$datos['fchRegistro']."', '".$datos['usuario']."', '".$datos['vendedor']."', '".$datos['grupo']."', '".$datos['supervisor']."', '".$datos['jefeVentas']."', '".$datos['estado']."', 'NO')";
 
-		    return $nuevoPros;
+		    return $ultimoPros;
 		  //   if ($sql2) {
 		  //   	$respuesta = array('cod' => '1', 'codProspecto'=> $cod_prospecto, 'consulta' => $consulta);
 				// return $respuesta;
