6928ab8e5e457a44a1be836eeea2517592153cb9
sebastian1907 <sebastian19071997@hotmail.com>
2020-02-20 17:29:32 -0500

    Update creaDatosSideBarContrato.php

--- extensiones/captcha/creaDatosSideBarContrato.php
+++ extensiones/captcha/creaDatosSideBarContrato.php
@@ -124,14 +124,14 @@ while($key = $db->recorrer($sql)){
                                 <i style="color: #3DB231;" class="fa fa-check"></i>
                             </button>';
             }else if ($key["flg_resuelto"] == "SI" || $key["flg_anulado"] == "SI") {
-                $buttons .= '';
+                $buttons .= '<h6>aca1</h6>';
             }else{
                 $buttons .= '<button type="button" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" data-container="body" data-placement="top" title="Activar" data-original-title="Activar" onclick="preActivarContrato("'.$numContrato.'","'.$codServicio.'")">
                                 <i class="fa fa-check"></i>
                             </button>';
             }
         }else{
-            $buttons .= '';
+            $buttons .= '<h6>aca2</h6>';
         }
 
 
