ab2d22a035e39d666255224bde93739f6fa4e0a6
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-02-21 14:57:11 -0500

    Update modificacionContrato.js

--- vista/js/modificacionContrato.js
+++ vista/js/modificacionContrato.js
@@ -2286,7 +2286,8 @@ function modificaContrato(){
     }else{
         var li_tot_cronograma = 0;
         li_total_cuotas_aux = buscaMaxCuotas(ls_tipo_ctt,ls_tipo_programa,ls_contrato,li_ref);
-        li_total_cuotas = li_total_cuotas_aux[0];
+        console.log(li_total_cuotas_aux);
+        // li_total_cuotas = li_total_cuotas_aux[0];
     }// End If is_cronograma
 
     if( is_cronograma_foma == 'SI'){     
@@ -2310,7 +2311,7 @@ function modificaContrato(){
 
          // -- Total -- //
         li_total_cuotas_aux = buscaMaxCuotas(ls_tipo_ctt,ls_tipo_programa,ls_contrato,li_ref);
-        li_total_cuotas = li_total_cuotas_aux[0];
+        // li_total_cuotas = li_total_cuotas_aux[0];
 
     }//End If is_cronograma_foma
      
@@ -2333,7 +2334,8 @@ function modificaContrato(){
         var oCells = tablaVin.rows.item(li_i).cells;
         var ls_servicio = oCells.item(0).innerHTML.trim();
         var lde_costo_carencia_aux = buscaCostoCarencia(ls_tipo_ctt,ls_tipo_programa,ls_contrato,ls_servicio);
-        lde_costo_carencia = lde_costo_carencia_aux[0];
+         console.log(lde_costo_carencia_aux);
+        // lde_costo_carencia = lde_costo_carencia_aux[0];
                    
         // -- Replica Datos tab Gestion-- //
         $.ajax({
