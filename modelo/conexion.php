7dfc3c057ec8a389eb92eba597fb201eec415272
sebastian1907 <sebastian19071997@hotmail.com>
2020-02-25 13:25:46 -0500

    Update conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,19 +1,15 @@
 <?php
 class Conexion{
   private $link;
-  public function __construct(){
-    define('DB_HOST','209.45.50.210');
-    define('DB_USER','userclient');
-    define('DB_PASS','asociados517');
-    define('DB_NAME','BDUS_CK000050_BE01');
-    //  $connection = array("Database"=>"BDUS_CK000050_PRE00","UID"=>"userclient","PWD"=>"asociados517",'CharacterSet' => 'UTF-8');
-    // $this->link = mssql_connect("209.45.50.210",$connection);
+  private $nombreBD;
+    public function __construct($nombreBD=''){
+    $nombreBD = ($nombreBD!='') ? $nombreBD : $_SESSION["nameBd"];
     $this->link = mssql_connect(DB_HOST,DB_USER,DB_PASS);
-    if(!$this->link){
+      if(!$this->link){
           echo 'No se pudo conectar';
           exit;
       }
-      if(!mssql_select_db(DB_NAME, $this->link)){
+      if(!mssql_select_db($nombreBD, $this->link)){
           echo 'No se pudo conectar';
           exit;
       }
@@ -28,9 +24,9 @@ class Conexion{
       return mssql_num_rows($query);
   }
   public function liberar($query){
-     return mssql_free_result($query);;
+    return mssql_free_result($query);
   }
-   public function cerrar(){
+  public function cerrar(){
     return mssql_close($this->link);
   }
   public function inicioTransaccion(){
@@ -45,8 +41,4 @@ class Conexion{
   public function rollback(){
     return true;
   }
-  public function validar($stmt){
-
-    return mssql_rows_affected($stmt);
-  }
-}
+}//class Conexion
