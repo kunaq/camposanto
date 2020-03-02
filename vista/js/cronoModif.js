33ecf6cb58fbbfa3b2ff8cbc41a2f55f48d4defc
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-02 12:43:37 -0500

    Update cronoModif.js

--- vista/js/cronoModif.js
+++ vista/js/cronoModif.js
@@ -222,10 +222,8 @@ function cronogramaModifi(){
             return;
         }
     }
-    console.log('ldt_emision',ldt_emision);
-    console.log('ldt_fch_ven',ldt_fch_ven);
-    console.log('ldt_fch_ven1',ldt_emision < ldt_fch_ven);
-    if ( ldt_emision < ldt_fch_ven && (ldt_emision != '' || ldt_emision != null) ){
+
+    if ( ldt_emision > ldt_fch_ven && (ldt_emision != '' || ldt_emision != null) ){
         swal({
             title: "",
             text: "La fecha del primer vencimiento debe ser mayor a la fecha de emisión del contrato.",
