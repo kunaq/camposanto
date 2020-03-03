5409094b90f0878377ccb348742b7abf18a047ca
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-03 12:49:38 -0500

    Update cronoModif.js

--- vista/js/cronoModif.js
+++ vista/js/cronoModif.js
@@ -674,20 +674,8 @@ function cronogramaModifi(){
                      
                     lde_igv_cuota    = lde_cuota - lde_capital_cuota;
                     lde_amortizacion = ( lde_cuota - lde_igv_cuota ) - lde_interes;
-
                     
                     lda_vencimiento = editar_fecha_30(lda_vencimiento, '+30', 'd', "",ldt_fch_ven1,0);
-                    console.log('lda_vencimiento fun', lda_vencimiento);
-                    // aux_dia = ldt_fch_ven.getDate();
-                    // aux_mes1 = ldt_fch_ven.setMonth(ldt_fch_ven.getMonth() + 1);
-                    // var aux_mes = ldt_fch_ven.getMonth();
-                    // aux_anio = ldt_fch_ven.getFullYear();
-                    // if(aux_mes == '0'){
-                    //   aux_mes = '12';
-                    //   aux_anio = ldt_fch_ven.getFullYear()-1;
-                    // }               
-                    // lda_vencimiento = aux_dia+'/'+aux_mes+'/'+aux_anio;
-
                   
                     // -- Saldos -- //
                   
@@ -784,15 +772,16 @@ function cronogramaModifi(){
 
                 }
                 
-                aux_dia = ldt_fch_ven.getDate();
-                aux_mes1 = ldt_fch_ven.setMonth(ldt_fch_ven.getMonth() + 1);
-                var aux_mes = ldt_fch_ven.getMonth();
-                aux_anio = ldt_fch_ven.getFullYear();
-                if(aux_mes == '0'){
-                  aux_mes = '12';
-                  aux_anio = ldt_fch_ven.getFullYear()-1;
-                }               
-                lda_vencimiento = aux_dia+'/'+aux_mes+'/'+aux_anio;
+                // aux_dia = ldt_fch_ven.getDate();
+                // aux_mes1 = ldt_fch_ven.setMonth(ldt_fch_ven.getMonth() + 1);
+                // var aux_mes = ldt_fch_ven.getMonth();
+                // aux_anio = ldt_fch_ven.getFullYear();
+                // if(aux_mes == '0'){
+                //   aux_mes = '12';
+                //   aux_anio = ldt_fch_ven.getFullYear()-1;
+                // }               
+                // lda_vencimiento = aux_dia+'/'+aux_mes+'/'+aux_anio;
+                lda_vencimiento = editar_fecha_30(lda_vencimiento, '+30', 'd', "",ldt_fch_ven1,0);
 
               
                 // -- Datos -- //
