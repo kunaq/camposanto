bf3b0aa112ba0fec0a53ab8f35e225b9d5959f7e
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-03 09:25:31 -0500

    Update cronoModif.js

--- vista/js/cronoModif.js
+++ vista/js/cronoModif.js
@@ -670,7 +670,7 @@ function cronogramaModifi(){
                     lde_igv_cuota    = lde_cuota - lde_capital_cuota;
                     lde_amortizacion = ( lde_cuota - lde_igv_cuota ) - lde_interes;
                     aux_dia = ldt_fch_ven.getDate();
-                    aux_mes = ldt_fch_ven.getMonth()
+                    aux_mes = ldt_fch_ven.getMonth();
                     if(ldt_fch_ven.getMonth() == 1){
                         aux_dia2 = ldt_fch_ven.setDate(ldt_fch_ven.getDate()+30);
                         aux_dia = ldt_fch_ven.getDate();
