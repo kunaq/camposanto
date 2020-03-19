c6f8a3e8b983a72ac7e303d4cc55455c1da1751f
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-19 18:54:12 -0500

    Update cronoModif.js

--- vista/js/cronoModif.js
+++ vista/js/cronoModif.js
@@ -475,9 +475,9 @@ function cronogramaModifi(){
                                                               
                             if( ls_tipo_cuota != 'CUI' ){
 
-                                lde_sumcapital = lde_sumcapital +  $("#imp_principal_2_"+li_i).val();
+                                lde_sumcapital = lde_sumcapital + pasaAnumero($("#imp_principal_2_"+li_i).val());
                                 console.log(li_i,lde_sumcapital);
-                                lde_sumtotal = lde_sumtotal + $("#imp_total_2_"+li_i).val();
+                                lde_sumtotal = lde_sumtotal + pasaAnumero($("#imp_total_2_"+li_i).val());
 
                             }            
                         }
