2dc96a5d89e4ba420380115450ff855fbe846cd2
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-26 16:54:14 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -345,7 +345,7 @@ function validaModifArbol(){
 //     OpenWithParm(w_vta_rsp_mto_arbol_vendedor, estructura)
 //     dw_detalle.Retrieve(ls_codigo, li_anno)
     buscanombre('dscTrabModConfArbVen',ls_codigo);
-    $("#periodoConfTraArbVen").val(ls_periodo).trigger('change');
+    $("#periodoConfTraArbVen").val(ls_periodo);
     codGrupo = $("#codGrupoArbVen").val();
     codComi = $("#codComiArbVen").val();
     codJefe = $("#codSupVenArbVen").val();
