78f2ef2aa8d91a3efb8e46aada0f821df5d792c1
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-30 01:46:13 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -450,7 +450,7 @@ function modificaArbol(){
 
     var ls_flg_supervisor = 'NO';
     var ls_flg_jefe = 'NO';
-    var is_opcion = 'INS';
+    var is_opcion = 'MOD';
 
     var is_codigo = $("#cod_trabajador").val();
 
@@ -601,7 +601,7 @@ function modificaArbol(){
         text: "¿Esta seguro que desea modificar el registro seleccionado?",
         type: "question",
         showCancelButton:!0,
-        confirmButtonText: "Anular",
+        confirmButtonText: "Aceptar",
         cancelButtonText:"Cancelar"
     }).then(function(){
         setTimeout(function () { 
@@ -612,7 +612,7 @@ function modificaArbol(){
                 url:"ajax/ArbolVendedores.ajax.php",
                 method: "POST",
                 dataType: 'json',
-                data: {'codTrabajador':cod, 'anno' : li_anno, 'tipo_periodo' : ls_tipo, 'periodo' : ls_periodo, 'grupo' : ls_grupo, 'tipo_comisionista' : ls_tipo_comisionista, 'supervisor' : ls_supervisor, 'jefe' : ls_jefe, 'flg_supervisor' : ls_flg_supervisor, 'flg_jefe' : ls_flg_jefe, 'accion':'modificar'},
+                data: {'codTrabajador':is_codigo, 'anno' : li_anno, 'tipo_periodo' : ls_tipo, 'periodo' : ls_periodo, 'grupo' : ls_grupo, 'tipo_comisionista' : ls_tipo_comisionista, 'supervisor' : ls_supervisor, 'jefe' : ls_jefe, 'flg_supervisor' : ls_flg_supervisor, 'flg_jefe' : ls_flg_jefe, 'accion':'modificar'},
                 success: function(respuesta){
                     if(respuesta == true){
                         swal({
