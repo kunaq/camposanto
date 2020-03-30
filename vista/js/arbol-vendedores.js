1c00265f1d004de18b1de5a259330c779a786c57
mirellyagv <mirellyagv@gmail.com>
2020-03-30 04:48:25 -0500

    a

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -48,16 +48,16 @@ function creaTablaTrabajadoresArbVend(){
 creaTablaTrabajadoresArbVend();
 
 //$("#SelAct").change(
-function selec1(a)
+function selec1()
 	{
 		$( "li" ).each(function() {
-			if (a=="1") 
+			if ($("#SelAct").value()=="1") 
 			{
 				$("#listaTrabArbVen").show(".act_SI");
 				$("#listaTrabArbVen").hidden(".act_NO");
 				return;
 				
-			} else if (a=="2") 
+			} else if ($("#SelAct").value()=="2") 
 			{
 				$("#listaTrabArbVen").hidden(".act_SI");
 				$("#listaTrabArbVen").show(".act_NO");
