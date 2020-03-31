8105d8312b5b0bb6aca463099664002450376ed1
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-30 22:42:10 -0500

    core

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -159,6 +159,7 @@ $_SESSION['captcha'] = simple_php_captcha();
     	<script src="vista/js/login.js" type="text/javascript"></script>
     	<script src="vista/js/lafuncion.js" type="text/javascript"></script>
     	<script src="vista/js/cronoModif.js" type="text/javascript"></script>
+    	<script src="vista/js/core.js" type="text/javascript"></script>
     	<?php
         if(isset($_GET["ruta"])){
             if($_GET["ruta"] == 'wizard' ||
