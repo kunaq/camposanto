8ac946689ed33e941bf7c4aaf1a5fb1c4ed3e1f9
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-31 12:25:58 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -636,26 +636,20 @@ function modificaArbol(){
     arregloValida.forEach(function(word) {
       console.log(word);
       if (word === valor) {
-        swal({
+        validaPeriodo = 1;
+      }
+    });
+
+
+    if (validaPeriodo == 1){
+         swal({
             title: "Error",
             text: "El trabajador ya se encuentra configurado para el periodo seleccionado.",
             type: "error",
             confirmButtonText: "Aceptar",
           });
         return;
-      }
-    });
-
-
-    // if (validaPeriodo == 1){
-    //      swal({
-    //         title: "Error",
-    //         text: "El trabajador ya se encuentra configurado para el periodo seleccionado.",
-    //         type: "error",
-    //         confirmButtonText: "Aceptar",
-    //       });
-    //     return;
-    // }
+    }
 
     // -- Valida -- //
 
