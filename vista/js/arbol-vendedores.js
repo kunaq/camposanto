c164bed6fcda6803430486aeffc443c49e91afaa
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-30 23:10:29 -0500

    Update arbol-vendedores.js

--- vista/js/arbol-vendedores.js
+++ vista/js/arbol-vendedores.js
@@ -8,6 +8,28 @@ $("#cttoResArbVen").on('click', function(){
 	document.getElementById("tituloCttArbVen").innerHTML = 'Resolución';
 });
 
+$("#nombreTrabajador").on("keyup", function() {
+
+  var patron = $(this).val();
+  // si el campo está vacío
+  if (patron == "") {
+    // mostrar todos los elementos
+    $(".itemLista").css("display", "list-item");
+    // si tiene valores, realizar la búsqueda
+  } else {
+    // atravesar la lista
+    $(".itemLista").each(function() {
+      if ($(this).text().indexOf(patron) < 0) {
+        // si el texto NO contiene el patrón de búsqueda, esconde el elemento
+        $(this).css("display", "none");
+      } else {
+        // si el texto SÍ contiene el patrón de búsqueda, muestra el elemento
+        $(this).css("display", "list-item");
+      }
+    });
+  }
+});
+
 $( "#edoTraArbVen" ).change(function (){
     if ($("#edoTraArbVen").val()=="2") 
     {
