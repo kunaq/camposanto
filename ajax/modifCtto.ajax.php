45afdbb5a227168a8bf3109cbe127b3d167559bf
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-03-31 19:17:39 -0500

    cc

--- ajax/modifCtto.ajax.php
+++ ajax/modifCtto.ajax.php
@@ -6,17 +6,17 @@ require_once "../modelo/modifCtto.modelo.php";
 class AjaxModifCtto{
 	public function ajaxBuscaCtto(){
 		$respuesta = ControladorModifCtto::ctrBuscaCtto();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_generacion"] = dateFormat($respuesta[$key]["fch_generacion"]);
-			$respuesta[$key]["fch_anulacion"] = ($respuesta[$key]["fch_anulacion"] != '') ? dateFormat($respuesta[$key]["fch_anulacion"]) : '';
-			$respuesta[$key]["fch_emision"] = ($respuesta[$key]["fch_emision"] != '') ? dateFormat($respuesta[$key]["fch_emision"]) : '';
-			$respuesta[$key]["fch_activacion"] = ($respuesta[$key]["fch_activacion"] != '') ? dateFormat($respuesta[$key]["fch_activacion"]) : '';
-			$respuesta[$key]["fch_resolucion"] = ($respuesta[$key]["fch_resolucion"] != '') ? dateFormat($respuesta[$key]["fch_resolucion"]) : '';
-			$respuesta[$key]["fch_primer_vencimiento"] = ($respuesta[$key]["fch_primer_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_primer_vencimiento"]) : '';
-			$respuesta[$key]["fch_termino_carencia"] = ($respuesta[$key]["fch_termino_carencia"] != '') ? dateFormat($respuesta[$key]["fch_termino_carencia"]) : '';
-			$respuesta[$key]["fch_transferencia"] = ($respuesta[$key]["fch_transferencia"] != '') ? dateFormat($respuesta[$key]["fch_transferencia"]) : '';
-			$respuesta[$key]["fch_real_generacion"] = ($respuesta[$key]["fch_real_generacion"] != '') ? dateFormat($respuesta[$key]["fch_real_generacion"]) : '';
-		}
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_generacion"] = dateFormat($respuesta[$key]["fch_generacion"]);
+		// 	$respuesta[$key]["fch_anulacion"] = ($respuesta[$key]["fch_anulacion"] != '') ? dateFormat($respuesta[$key]["fch_anulacion"]) : '';
+		// 	$respuesta[$key]["fch_emision"] = ($respuesta[$key]["fch_emision"] != '') ? dateFormat($respuesta[$key]["fch_emision"]) : '';
+		// 	$respuesta[$key]["fch_activacion"] = ($respuesta[$key]["fch_activacion"] != '') ? dateFormat($respuesta[$key]["fch_activacion"]) : '';
+		// 	$respuesta[$key]["fch_resolucion"] = ($respuesta[$key]["fch_resolucion"] != '') ? dateFormat($respuesta[$key]["fch_resolucion"]) : '';
+		// 	$respuesta[$key]["fch_primer_vencimiento"] = ($respuesta[$key]["fch_primer_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_primer_vencimiento"]) : '';
+		// 	$respuesta[$key]["fch_termino_carencia"] = ($respuesta[$key]["fch_termino_carencia"] != '') ? dateFormat($respuesta[$key]["fch_termino_carencia"]) : '';
+		// 	$respuesta[$key]["fch_transferencia"] = ($respuesta[$key]["fch_transferencia"] != '') ? dateFormat($respuesta[$key]["fch_transferencia"]) : '';
+		// 	$respuesta[$key]["fch_real_generacion"] = ($respuesta[$key]["fch_real_generacion"] != '') ? dateFormat($respuesta[$key]["fch_real_generacion"]) : '';
+		// }
 		echo json_encode($respuesta);
 	}//function ajaxBuscaCtto
 	public function ajaxBuscaDatosServicio(){
@@ -31,29 +31,29 @@ class AjaxModifCtto{
 	}//ajaxBuscaDatosServicio
 	public function ajaxBuscaServPpal(){
 		$respuesta = ControladorModifCtto::ctrBuscaServPpal();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_generacion"] = ($respuesta[$key]["fch_generacion"] != '') ? dateFormat($respuesta[$key]["fch_generacion"]) : '';
-			$respuesta[$key]["fch_anulacion"] = ($respuesta[$key]["fch_anulacion"] != '') ? dateFormat($respuesta[$key]["fch_anulacion"]) : '';
-			$respuesta[$key]["fch_emision"]= ($respuesta[$key]["fch_emision"] != '') ? dateFormat($respuesta[$key]["fch_emision"]) : '';
-			$respuesta[$key]["fch_activacion"] = ($respuesta[$key]["fch_activacion"] != '') ? dateFormat($respuesta[$key]["fch_activacion"]) : '';
-			$respuesta[$key]["fch_resolucion"] = ($respuesta[$key]["fch_resolucion"] != '') ? dateFormat($respuesta[$key]["fch_resolucion"]) : '';
-		}	
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_generacion"] = ($respuesta[$key]["fch_generacion"] != '') ? dateFormat($respuesta[$key]["fch_generacion"]) : '';
+		// 	$respuesta[$key]["fch_anulacion"] = ($respuesta[$key]["fch_anulacion"] != '') ? dateFormat($respuesta[$key]["fch_anulacion"]) : '';
+		// 	$respuesta[$key]["fch_emision"]= ($respuesta[$key]["fch_emision"] != '') ? dateFormat($respuesta[$key]["fch_emision"]) : '';
+		// 	$respuesta[$key]["fch_activacion"] = ($respuesta[$key]["fch_activacion"] != '') ? dateFormat($respuesta[$key]["fch_activacion"]) : '';
+		// 	$respuesta[$key]["fch_resolucion"] = ($respuesta[$key]["fch_resolucion"] != '') ? dateFormat($respuesta[$key]["fch_resolucion"]) : '';
+		// }	
 	echo json_encode($respuesta);
 	}//ajaxBuscaServPpal
 	public function ajaxBuscaDsctoXCtto(){
 		$respuesta = ControladorModifCtto::ctrBuscaDsctoXCtto();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
-		}	
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
+		// }	
 	echo json_encode($respuesta);
 	}//ajaxBuscaDsctoXCtto
 	public function ajaxBuscaEndXCtto(){
 		$respuesta = ControladorModifCtto::ctrBuscaEndXCtto();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
-			$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
-			$respuesta[$key]["fch_cancelacion"] = ($respuesta[$key]["fch_cancelacion"] != '') ? dateFormat($respuesta[$key]["fch_cancelacion"]) : '';
-		}	
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
+		// 	$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
+		// 	$respuesta[$key]["fch_cancelacion"] = ($respuesta[$key]["fch_cancelacion"] != '') ? dateFormat($respuesta[$key]["fch_cancelacion"]) : '';
+		// }	
 	echo json_encode($respuesta);
 	}//ajaxBuscaEndXCtto
 	public function ajaxBuscaCliente(){
@@ -67,23 +67,23 @@ class AjaxModifCtto{
 	}//ajaxBuscaCliente
 	public function ajaxBuscaCronograma(){
 		$respuesta = ControladorModifCtto::ctrBuscaCronograma();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
-		}
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
+		// }
 		echo json_encode($respuesta);
 	}//ajaxBuscaCronograma
 	public function ajaxBuscaCronogramaFOMA(){
 		$respuesta = ControladorModifCtto::ctrBuscaCronogramaFOMA();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
-		}
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_vencimiento"] = ($respuesta[$key]["fch_vencimiento"] != '') ? dateFormat($respuesta[$key]["fch_vencimiento"]) : '';
+		// }
 		echo json_encode($respuesta);
 	}//ajaxBuscaCronogramaFOMA
 	public function ajaxBuscaObservaciones(){
 		$respuesta = ControladorModifCtto::ctrBuscaObservaciones();
-		foreach ($respuesta as $key => $value) {
-			$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
-		}
+		// foreach ($respuesta as $key => $value) {
+		// 	$respuesta[$key]["fch_registro"] = ($respuesta[$key]["fch_registro"] != '') ? dateTimeFormat($respuesta[$key]["fch_registro"]) : '';
+		// }
 		echo json_encode($respuesta);
 	}//ajaxBuscaObservaciones
 	public function ajaxValUsoServ(){
--- modelo/modifCtto.modelo.php
+++ modelo/modifCtto.modelo.php
@@ -8,6 +8,15 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT $tabla.*, $tabla2.dsc_cliente, $tabla3.*, $tabla4.dsc_camposanto, $tabla5.dsc_area, $tabla6.dsc_plataforma, $tabla7.dsc_tipo_espacio, $tabla8.dsc_tipo_servicio, $tabla8.flg_afecto_igv, $tabla8.flg_prevencion, $tablaViServ.flg_principal FROM $tabla LEFT JOIN $tabla2 ON $tabla.cod_cliente = $tabla2.cod_cliente LEFT JOIN $tabla3 ON $tabla.cod_contrato = $tabla3.cod_contrato LEFT JOIN $tabla4 ON $tabla4.cod_camposanto = $tabla.cod_empresa LEFT JOIN $tabla5 ON $tabla5.cod_area_plataforma = $tabla3.cod_areaplataforma_actual LEFT JOIN  $tabla6 ON $tabla6.cod_plataforma = $tabla3.cod_plataforma_actual LEFT JOIN $tabla7 ON $tabla7.cod_tipo_espacio = $tabla3.cod_tipoespacio_actual LEFT JOIN $tabla8 ON $tabla8.cod_tipo_servicio = $tabla.cod_tipo_servicio LEFT JOIN $tablaViServ ON ($tablaViServ.cod_contrato = $tabla.cod_contrato AND $tablaViServ.num_servicio = $tabla.num_servicio AND $tablaViServ.cod_localidad = $tabla.cod_localidad) WHERE $tabla.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND $tabla.flg_fondo_mantenimiento = 'NO'");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+	    		$key["fch_generacion"] = dateFormat($key["fch_generacion"]);
+				$key["fch_anulacion"] = ($key["fch_anulacion"] != '') ? dateFormat($key["fch_anulacion"]) : '';
+				$key["fch_emision"] = ($key["fch_emision"] != '') ? dateFormat($key["fch_emision"]) : '';
+				$key["fch_activacion"] = ($key["fch_activacion"] != '') ? dateFormat($key["fch_activacion"]) : '';
+				$key["fch_resolucion"] = ($key["fch_resolucion"] != '') ? dateFormat($key["fch_resolucion"]) : '';
+				$key["fch_primer_vencimiento"] = ($key["fch_primer_vencimiento"] != '') ? dateFormat($key["fch_primer_vencimiento"]) : '';
+				$key["fch_termino_carencia"] = ($key["fch_termino_carencia"] != '') ? dateFormat($key["fch_termino_carencia"]) : '';
+				$key["fch_transferencia"] = ($key["fch_transferencia"] != '') ? dateFormat($key["fch_transferencia"]) : '';
+				$key["fch_real_generacion"] = ($key["fch_real_generacion"] != '') ? dateFormat($key["fch_real_generacion"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -29,6 +38,11 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT $tablaCttoSvcio.cod_servicio_principal, $tablaCttoSvcio.num_ctd, $tablaCttoSvcio.imp_precio_venta, $tablaCttoSvcio.imp_min_inhumar, $tablaCttoSvcio.imp_subtotal, $tablaCttoSvcio.imp_igv, $tablaCttoSvcio.imp_total,$tablaMaSvcio.dsc_servicio FROM $tablaCttoSvcio INNER JOIN $tablaMaSvcio ON $tablaCttoSvcio.cod_servicio_principal = $tablaMaSvcio.cod_servicio WHERE $tablaCttoSvcio.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND $tablaCttoSvcio.num_servicio = $num_servicio");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+	    		$key["fch_generacion"] = ($key["fch_generacion"] != '') ? dateFormat($key["fch_generacion"]) : '';
+				$key["fch_anulacion"] = ($key["fch_anulacion"] != '') ? dateFormat($key["fch_anulacion"]) : '';
+				$key["fch_emision"]= ($key["fch_emision"] != '') ? dateFormat($key["fch_emision"]) : '';
+				$key["fch_activacion"] = ($key["fch_activacion"] != '') ? dateFormat($key["fch_activacion"]) : '';
+				$key["fch_resolucion"] = ($key["fch_resolucion"] != '') ? dateFormat($key["fch_resolucion"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -41,6 +55,7 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT $tablaDscto.flg_tasa, $tablaMaDcsto.dsc_tipo_descuento, $tablaDscto.flg_libre, $tablaDscto.cod_usuario, $tablaDscto.fch_registro, $tablaDscto.imp_valor, $tablaDscto.imp_dscto FROM $tablaDscto LEFT JOIN $tablaMaDcsto ON $tablaMaDcsto.cod_tipo_descuento = $tablaDscto.cod_tipo_descuento WHERE $tablaDscto.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND $tablaDscto.num_servicio = $num_servicio");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    			$key["fch_registro"] = ($key["fch_registro"] != '') ? dateTimeFormat($key["fch_registro"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -53,6 +68,9 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT $tablaMaEnd.dsc_entidad, $tablaEndoso.imp_valor, $tablaEndoso.imp_saldo, $tablaEndoso.imp_total_emitido, $tablaEndoso.cod_usuario, $tablaEndoso.fch_registro, $tablaEndoso.cod_estado, $tablaEndoso.fch_vencimiento, $tablaEndoso.fch_cancelacion FROM $tablaEndoso INNER JOIN $tablaMaEnd ON $tablaMaEnd.cod_entidad = $tablaEndoso.cod_entidad WHERE $tablaEndoso.cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND $tablaEndoso.num_servicio = $num_servicio");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    			$key["fch_registro"] = ($key["fch_registro"] != '') ? dateTimeFormat($key["fch_registro"]) : '';
+				$key["fch_vencimiento"] = ($key["fch_vencimiento"] != '') ? dateFormat($key["fch_vencimiento"]) : '';
+				$key["fch_cancelacion"] = ($key["fch_cancelacion"] != '') ? dateFormat($key["fch_cancelacion"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -74,6 +92,7 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT num_cuota, cod_estadocuota, fch_vencimiento, imp_principal, imp_interes, imp_igv, imp_saldo, imp_total, cod_tipo_cuota FROM $tablaCronograma WHERE cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND num_refinanciamiento = $num_refinanciamiento AND num_cuota != 0 AND cod_tipo_cuota != 'FMA' ORDER BY num_cuota ASC");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    			$key["fch_vencimiento"] = ($key["fch_vencimiento"] != '') ? dateFormat($key["fch_vencimiento"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -86,6 +105,7 @@ class ModeloModifCtto{
 		$sql = $db->consulta("SELECT num_cuota, cod_estadocuota, fch_vencimiento, imp_saldo, imp_total FROM $tablaCronograma WHERE cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND num_refinanciamiento = $num_refinanciamiento AND num_cuota != 0 AND cod_tipo_cuota = 'FMA' ORDER BY num_cuota ASC");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    			$key["fch_vencimiento"] = ($key["fch_vencimiento"] != '') ? dateFormat($key["fch_vencimiento"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
@@ -98,6 +118,7 @@ class ModeloModifCtto{
 		 $sql = $db->consulta("SELECT $tablaObservacion.num_linea, $tablaObservacion.dsc_observacion, $tablaObservacion.cod_usuario, $tablaObservacion.fch_registro, $tablaObservacion.flg_automatico FROM $tablaObservacion WHERE cod_contrato LIKE (RIGHT('0000000000'+'$codCtto',10)) AND num_servicio = $num_servicio");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    			$key["fch_registro"] = ($key["fch_registro"] != '') ? dateTimeFormat($key["fch_registro"]) : '';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
