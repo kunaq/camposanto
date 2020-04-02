e3c0933a09e3f82512f5a0b8c18900935236f3e4
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-04-02 16:38:50 -0500

    Update visorPrestacionServicio.js

--- vista/js/visorPrestacionServicio.js
+++ vista/js/visorPrestacionServicio.js
@@ -15,3 +15,43 @@ function mostrarSidebar(numContrato,codServicio){
 function hideSidebar(){
     $("#m_quick_sidebar-contrato").removeClass("m-quick-sidebar-contrato--on");
 }
+
+$("#fechVPS").on("change", function(){
+	var fecha = $(this).val();
+	console.log(fecha);
+	// $.ajax({
+ //        method:'POST',
+ //        url: 'ajax/VPS.ajax.php',
+ //        dataType: 'json',
+ //        data: {'entrada':'verTrabajadores'},
+ //        success : function(respuesta){
+ //        	// console.log(respuesta);
+ //        	var classPeriodo = '';
+ //        	var color = '';
+ //            $.each(respuesta,function(index,value){
+ //                if(index == 0){
+ //                    classPeriodo = 'liListaKqPstImpar';
+ //                }else if(index%2 == 0){
+ //                    classPeriodo = 'liListaKqPstImpar';
+ //                }else{
+ //                    classPeriodo = 'liListaKqPstPar';
+ //                }
+ //                if(value['flg_activo'] == 'SI'){
+ //                	color = 'black';
+ //                }else{
+ //                	color = 'red';
+ //                }
+ //                $("#listaTrabArbVen").append(
+ //                    '<li class="nav-item '+classPeriodo+' itemLista act_'+value['flg_activo']+'">'+
+ //                        '<a href="#" class="btnVerTrabArbVen" codTrabajador="'+value['cod_trabajador']+'" flg_activo="'+value['flg_activo']+'">'+
+ //                        	'<div class="row" style = "color:'+color+'">'+
+	// 							'<div class="col-md-4">'+value['cod_trabajador']+'</div>'+
+	// 							'<div class="col-md-8">'+value['dsc_apellido_paterno']+' '+value['dsc_apellido_materno']+', '+value['dsc_nombres']+'</div>'+
+	// 						'</div>'+
+ //                        '</a>'+
+ //                    '</li>'
+ //                );//append
+ //            });//each
+ //        }//success
+ //    });//ajax
+});
