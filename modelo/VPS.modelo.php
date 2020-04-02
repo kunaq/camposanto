70f1014f2f0a216a0c6335f6e4f2b4f52d9b17cd
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-04-02 17:48:57 -0500

    Update VPS.modelo.php

--- modelo/VPS.modelo.php
+++ modelo/VPS.modelo.php
@@ -8,6 +8,7 @@ class ModeloVPS{
 		$sql = $db->consulta("SELECT $tabla1.dsc_tipo_autorizacion, $tabla1.dsc_prefijo, $tabla2.cod_localidad, $tabla2.num_uso_servicio, $tabla2.dsc_apellido_paterno + ' ' +$tabla2.dsc_apellido_materno + ', ' + $tabla2.dsc_nombres AS dsc_nombres, $tabla3.dsc_autorizacion, $tabla2.fch_servicio, $tabla2.cod_tipo_autorizacion FROM $tabla2 INNER JOIN $tabla3 ON $tabla3.cod_estado_autorizacion = $tabla2.cod_estado_autorizacion INNER JOIN $tabla1 ON $tabla1.cod_tipo_autorizacion = $tabla2.cod_tipo_autorizacion WHERE $tabla3.flg_anulado = 'NO' AND CONVERT(DATE, $tabla2.fch_servicio,105) = CONVERT(DATE, '".$datos['fecha']."',105) AND $tabla2.cod_localidad = '".$datos['localidad']."'");
 		$datos = array();
     	while($key = $db->recorrer($sql)){
+    		$key["fch_servicio"] = ($key["fch_servicio"] != '') ? dateTimeFormat($key["fch_servicio"]) : '-';
 	    		$datos[] = arrayMapUtf8Encode($key);
 			}
 		return $datos;
