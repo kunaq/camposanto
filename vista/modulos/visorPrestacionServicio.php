177a192fad42799b0c83441606f5d12b7cce1a99
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-04-02 22:33:09 -0500

    xvcs

--- vista/modulos/visorPrestacionServicio.php
+++ vista/modulos/visorPrestacionServicio.php
@@ -151,73 +151,17 @@
 			<div class="tab-pane active m-scrollable" id="m_quick_sidebar_tabs_contrato" role="tabpanel">
 				<div class="row">
 					<div class="col-lg-12">
-						<table class="tableSideBar">
-							<tbody>
-								<tr>
-									<td class="leftText">Fecha Emisión</td>
-									<td class="datosSideBar" id="fchEmiSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Fecha Activación</td>
-									<td class="datosSideBar" id="fchActSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Fecha Resolución</td>
-									<td class="datosSideBar" id="fchResSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Fecha Anulación</td>
-									<td class="datosSideBar" id="fchAnuSideBar"></td>
-								</tr>
-							</tbody>
-						</table>
+						<label>Tipo</label>
+						<input type="text" class="form-control form-control-sm m-input" id="dsc_autorizacionVPS" name="dsc_autorizacionVPS" disabled>
 					</div>
-					<div class="col-lg-12">&nbsp;</div>
-					<div class="col-lg-12">&nbsp;</div>
-					<div class="col-lg-12">
-						<label>Detalles</label>
-					</div>
-					<div class="col-lg-12">
-						<table class="tableSideBar">
-							<tbody>
-								<tr>
-									<td class="leftText">Cliente</td>
-									<td class="datosSideBar" id="clienteSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Tipo Necesidad</td>
-									<td class="datosSideBar" id="tipoNecSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Vendedor</td>
-									<td class="datosSideBar" id="vendedorSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Tipo Servicio</td>
-									<td class="datosSideBar" id="tipoServSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">N° Cuotas</td>
-									<td class="datosSideBar" id="numCuotasSideBar"></td>
-								</tr>
-								<tr>
-									<td class="leftText">Total</td>
-									<td class="datosSideBar" id="totalSideBar"></td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<div class="col-lg-12">&nbsp;</div>
-					<div class="col-lg-12">&nbsp;</div>
-					<div class="col-lg-7 button-box" id="buttons-box">
-					</div>
+				</div>
+				<div class="row">
 					<div class="col-lg-5 button-box">
-                            <a href="refinanciamiento" target="_blank" type="button" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" data-container="body" data-placement="top" title="Refinanciamiento" onclick="">
-                                <i class="fa fa-balance-scale"></i>
-                            </a>
-                            <a href="cambioTitular" target="_blank" type="button" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-container="body" data-placement="top" title="Resolver" data-original-title="Cambio de Titular" onclick="">
-                                <i class="fa fa-users"></i>
-                            </a>
+                        <a href="seguimientoContrato?localidad=00001&amp;contrato=0000001784" target="_blank">
+                        	<button type="button" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="m-tooltip" data-container="body" data-placement="top" title="Seguimiento">
+	                            <i class="la la-eye"></i>
+	                        </button>
+	                    </a>
 					</div>
 				</div>
 			</div>
